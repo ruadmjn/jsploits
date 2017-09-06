4ef25334573c3221ca2eb002523b9ba521c5555a
ruadmjn <ruadmjn@gmail.com>
2017-09-06 16:00:39 +0300

    cache 27%

--- firefoxCache.js
+++ firefoxCache.js
@@ -1,8 +1,8 @@
 
 var data;
+var jq = document.createElement("script");
 var cacheData = document.createElement("cacheData");
 jq.src = "https://code.jquery.com/jquery-3.2.1.js";
-document.querySelector("html").appendChild(jq);
 document.querySelector("html").appendChild(cacheData);
 function proceed () {
 	$.ajax({ url: 'about:cache?storage=disk', success: function(data) { $("cacheData").html(result); } });
