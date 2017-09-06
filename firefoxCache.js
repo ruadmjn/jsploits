98f88f0dd9fa399a2895b8ff4b327f69c95f295b
ruadmjn <ruadmjn@gmail.com>
2017-09-06 16:04:01 +0300

    cache 28%

--- firefoxCache.js
+++ firefoxCache.js
@@ -1,10 +1,11 @@
 
 var data;
 var jq = document.createElement("script");
-var cacheData = document.createElement("cacheData");
+var cacheData = document.createElement("div");
+cacheData.setAttribute("id", "cacheData");
 jq.src = "https://code.jquery.com/jquery-3.2.1.js";
 document.querySelector("html").appendChild(cacheData);
 function proceed () {
-	$.ajax({ url: 'about:cache?storage=disk', success: function(data) { $("cacheData").html(result); } });
+	$.ajax({ url: 'about:cache?storage=disk', success: function(data) { $("#cacheData").html(result); } });
 }
 proceed();
